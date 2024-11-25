4be57aa590ed056e61886957d1b1c2cf478d1a8b
itzAkmal97 <akmalm723@yahoo.com>
2024-11-25 12:04:15 +0500

    Added Login and Sign up Page UI

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,17 +1,51 @@
-import SignupPage from "../components/SignupPage"
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 function LoginPage() {
   return (
-    <div>
-        <h1>Log In</h1>
-        <form>
-            <input type="email" placeholder="Email" className=""/>
-            <input type="password" placeholder="Password" />
-            <button type="submit">Sign In</button>
-        </form>
+    <div className="px-4">
+      <h1 className="text-center text-2xl md:text-5xl mt-8 mb-4">
+        Log In
+      </h1>
+      <form>
+        <div className="flex flex-col gap-6 mt-16 md:flex md:flex-row md:justify-around">
+          <div className="flex flex-col gap-4 items-start justify-between w-full md:w-1/2">
+          <h2 className="text-2xl font-semibold mb-4">Log In</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+        />
+        <button
+          type="submit"
+          className="bg-black text-white font-semibold py-3 px-6 rounded-md hover:transform hover:scale-105 duration-300 ease-in-out mt-4"
+        >
+          Sign In
+        </button>
+          </div>
+        
+        <div className="flex flex-col gap-6 items-start md:max-w-lg">
+        <h2 className="text-2xl mt-6 font-bold">New Customer</h2>
+        <p>
+          Sign up for early Sale access plus tailored new arrivals, trends and
+          promotions. To opt out, click unsubscribe in our emails.
+        </p>
+
+        <button
+          className="bg-black text-white font-semibold py-3 px-6 rounded-md hover:transform hover:scale-105 duration-300 ease-in-out"
+        >
+          <NavLink to={"/signup"}>Register</NavLink>
+        </button>
+        </div>
+        
+        </div>
+       
+      </form>
     </div>
-    
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- src/components/SignupPage.tsx
+++ src/components/SignupPage.tsx
@@ -1,7 +1,52 @@
+import { NavLink } from "react-router-dom";
 function SignupPage() {
   return (
-    <div>SignupPage</div>
-  )
+    <div className="px-4 md:flex md:flex-col justify-center items-center">
+      <h1 className="text-center text-2xl md:text-5xl mt-8 mb-4">Register</h1>
+      <form className="flex flex-col gap-6 mt-16 w-full md:w-1/2">
+        <h2 className="text-2xl font-semibold mb-4">Register</h2>
+
+        <input
+          type="name"
+          placeholder="First Name"
+          className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+        />
+
+        <input
+          type="name"
+          placeholder="Last Name"
+          className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+        />
+
+        <p className="text-gray-500">Sign up for early Sale access plus tailored new arrivals, trends and promotions. To opt out, click unsubscribe in our emails.</p>
+
+        <button
+          type="submit"
+          className="bg-black text-white font-semibold py-3 px-6 rounded-md hover:transform hover:scale-105 duration-300 ease-in-out mt-4"
+        >
+          Register
+        </button>
+
+        <button
+          className="hover:bg-black hover:text-white border border-black font-semibold py-3 px-6 rounded-md hover:transform hover:scale-105 duration-300 ease-in-out"
+        >
+          <NavLink to={"/login"}>Login</NavLink>
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default SignupPage
+export default SignupPage;
