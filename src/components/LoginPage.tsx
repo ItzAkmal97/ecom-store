a2dc67025785fccc4a7ea9b449dfd7968ee007ea
itzAkmal97 <akmalm723@yahoo.com>
2024-11-25 13:48:24 +0500

    Added form validations

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,48 +1,76 @@
 import { NavLink } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+type LoginData = {
+  email: string;
+  password: string;
+};
 function LoginPage() {
+  const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginData>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = () => {
+    alert("User Logged in Successfully");
+  };
   return (
     <div className="px-4 py-16">
-      <h1 className="text-center text-3xl md:text-4xl mt-8 mb-4">
-        Log In
-      </h1>
-      <form>
+      <h1 className="text-center text-3xl md:text-4xl mt-8 mb-4">Log In</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col gap-6 mt-8 md:flex md:flex-row md:justify-around">
           <div className="flex flex-col gap-4 items-start justify-between w-full md:w-1/2">
-          <h2 className="text-2xl font-semibold mb-4">Log In</h2>
-        <input
-          type="email"
-          placeholder="Email"
-          className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
-        />
-        <button
-          type="submit"
-          className="bg-black text-white font-semibold py-3 px-6 rounded-md hover:transform hover:scale-105 duration-300 ease-in-out mt-4"
-        >
-          Sign In
-        </button>
+            <h2 className="text-2xl font-semibold mb-4">Log In</h2>
+            <input
+              type="email"
+              placeholder="Email"
+              className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+            <input
+              type="password"
+              placeholder="Password"
+              className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+              {...register("password")}
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+            <button
+              type="submit"
+              className="bg-black text-white font-semibold py-3 px-6 rounded-md hover:transform hover:scale-105 duration-300 ease-in-out mt-4"
+            >
+              Sign In
+            </button>
           </div>
-        
-        <div className="flex flex-col gap-6 items-start md:max-w-lg">
-        <h2 className="text-2xl mt-6 font-bold">New Customer</h2>
-        <p className="text-gray-500 font-semibold">
-          Sign up for early Sale access plus tailored new arrivals, trends and
-          promotions. To opt out, click unsubscribe in our emails.
-        </p>
 
-        <NavLink to={"/signup"}>
-          <button className="bg-black text-white font-semibold py-3 px-6 rounded-md hover:transform hover:scale-105 duration-300 ease-in-out mt-4">
-            Register
-          </button>
-        </NavLink>
+          <div className="flex flex-col gap-6 items-start md:max-w-lg">
+            <h2 className="text-2xl mt-6 font-bold">New Customer</h2>
+            <p className="text-gray-500 font-semibold">
+              Sign up for early Sale access plus tailored new arrivals, trends
+              and promotions. To opt out, click unsubscribe in our emails.
+            </p>
+
+            <NavLink to={"/signup"}>
+              <button className="bg-black text-white font-semibold py-3 px-6 rounded-md hover:transform hover:scale-105 duration-300 ease-in-out mt-4">
+                Register
+              </button>
+            </NavLink>
+          </div>
         </div>
-        
-        </div>
-       
       </form>
     </div>
   );
--- src/components/SignupPage.tsx
+++ src/components/SignupPage.tsx
@@ -1,35 +1,111 @@
 import { NavLink } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+// import { useState } from "react";
+
+type SignupData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 function SignupPage() {
+  const schema = yup.object().shape({
+    firstName: yup.string().required(),
+    lastName: yup.string().min(2).required(),
+    email: yup.string().email().required(),
+    password: yup
+      .string()
+      .required()
+      .min(8, "Password must be at least 8 characters long")
+      .max(128, "Password must be no more than 128 characters long")
+      .matches(
+        /^(?=.*[a-z])/,
+        "Password must contain at least one lowercase letter"
+      )
+      .matches(
+        /^(?=.*[A-Z])/,
+        "Password must contain at least one uppercase letter"
+      )
+      .matches(/^(?=.*[0-9])/, "Password must contain at least one number")
+      .matches(
+        /^(?=.*[!@#$%^&*()_+={};:<>?])/,
+        "Password must contain at least one special character"
+      ),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password")], "Passwords must match")
+      .required("Confirm Password is required"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<SignupData>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = () => {
+    alert("Form submitted successfully");
+    reset();
+  };
+
   return (
     <div className="px-4 sm:flex sm:flex-col sm:justify-center sm:items-center py-16">
       <h1 className="text-center text-3xl md:text-4xl mt-8 mb-4">Register</h1>
-      <form className="flex flex-col gap-6 mt-8 max-w-[500px]">
+      <form
+        className="flex flex-col gap-6 mt-8 max-w-[500px]"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <h2 className="text-2xl font-semibold mb-4">Register</h2>
 
         <input
-          type="name"
+          type="text"
           placeholder="First Name"
           className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+          {...register("firstName")}
         />
-
+        {errors.firstName && (
+          <p className="text-red-500">{"First name is required"}</p>
+        )}
         <input
-          type="name"
+          type="text"
           placeholder="Last Name"
           className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+          {...register("lastName")}
         />
-
+        {errors.lastName && (
+          <p className="text-red-500">{"Last name must be at least 2 characters and is required"}</p>
+        )}
         <input
           type="email"
           placeholder="Email"
           className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+          {...register("email")}
         />
-
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
         <input
           type="password"
           placeholder="Password"
           className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+          {...register("password")}
         />
-
+        {errors.password && (
+          <p className="text-red-500">{errors.password.message}</p>
+        )}
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          className="h-14 w-full border focus:border-black transition duration-500 ease-in-out p-2 rounded-md"
+          {...register("confirmPassword")}
+        />
+        {errors.confirmPassword && (
+          <p className="text-red-500">{errors.confirmPassword.message}</p>
+        )}
         <p className="text-gray-500 font-semibold">
           Sign up for early Sale access plus tailored new arrivals, trends and
           promotions. To opt out, click unsubscribe in our emails.
@@ -42,7 +118,6 @@ function SignupPage() {
           Register
         </button>
 
-        
         <NavLink to={"/login"}>
           <button className="w-full h-full hover:bg-black hover:text-white border border-black font-semibold py-3 px-6 rounded-md hover:transform hover:scale-105 duration-300 ease-in-out">
             Login
