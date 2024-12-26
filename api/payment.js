ed42abb7cd9852a11ec63d5c2399ce00b1e26876
ItzAkmal97 <146759340+ItzAkmal97@users.noreply.github.com>
2024-12-26 16:45:19 +0500

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -10,8 +10,7 @@ const stripe = new Stripe(
 app.use(
   cors({
     origin: [
-      "http://localhost:5000",
-      "http://localhost:5173",
+      "https://ecom-store-swart.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
